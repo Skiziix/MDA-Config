5606c444bb78db34cccdd8cb83d925a5afffc51d
bashfulmonkey <rearick.seth@gmail.com>
2023-12-21 11:12:43 -0500

    Named the class PowerAppEntity. Created a proper __init__ function.

--- BasicAuth.py
+++ BasicAuth.py
@@ -13,113 +13,118 @@ import msal
 
 config = json.load(open(sys.argv[1]))
 
-# If for whatever reason you plan to recreate same ClientApplication periodically,
-# you shall create one global token cache and reuse it by each ClientApplication
-global_token_cache = msal.TokenCache()  # The TokenCache() is in-memory.
-    # See more options in https://msal-python.readthedocs.io/en/latest/#tokencache
 
-# Create a preferably long-lived app instance, to avoid the overhead of app creation
-global_app = msal.ConfidentialClientApplication(
-    config["client_id"], authority=config["authority"],
-    client_credential=config["secret"],
-    token_cache=global_token_cache,  # Let this app (re)use an existing token cache.
-        # If absent, ClientApplication will create its own empty token cache
-    )
+class PowerAppEntity:
+
+    def __init__(self, config):
+
+        # If for whatever reason you plan to recreate same ClientApplication periodically,
+        # you shall create one global token cache and reuse it by each ClientApplication
+        global_token_cache = msal.TokenCache()  # The TokenCache() is in-memory.
+            # See more options in https://msal-python.readthedocs.io/en/latest/#tokencache
+
+        # Create a preferably long-lived app instance, to avoid the overhead of app creation
+        self.global_app = msal.ConfidentialClientApplication(
+            config["client_id"], authority=config["authority"],
+            client_credential=config["secret"],
+            token_cache=global_token_cache,  # Let this app (re)use an existing token cache.
+                # If absent, ClientApplication will create its own empty token cache
+            )
 
 
-def acquire_token():
-    # Since MSAL 1.23, acquire_token_for_client(...) will automatically look up
-    # a token from cache, and fall back to acquire a fresh token when needed.
-    token = global_app.acquire_token_for_client(scopes=config["scope"])
+    def acquire_token(self):
+        # Since MSAL 1.23, acquire_token_for_client(...) will automatically look up
+        # a token from cache, and fall back to acquire a fresh token when needed.
+        token = self.global_app.acquire_token_for_client(scopes=config["scope"])
 
-    # Json model of attribute to be created
-    picklist_global_choice = {
-        "@odata.type": "Microsoft.Dynamics.CRM.PicklistAttributeMetadata",
-        "AttributeType": "Picklist",
-        "AttributeTypeName": {
-            "Value": "PicklistType"
-        },
-        "SourceTypeMask": 0,
-        "GlobalOptionSet@odata.bind": "/GlobalOptionSetDefinitions(b0a161bd-409f-ee11-be37-6045bd0064ab)",
-        "Description": {
-            "@odata.type": "Microsoft.Dynamics.CRM.Label",
-            "LocalizedLabels": [
-            {
+        # Json model of attribute to be created
+        picklist_global_choice = {
+            "@odata.type": "Microsoft.Dynamics.CRM.PicklistAttributeMetadata",
+            "AttributeType": "Picklist",
+            "AttributeTypeName": {
+                "Value": "PicklistType"
+            },
+            "SourceTypeMask": 0,
+            "GlobalOptionSet@odata.bind": "/GlobalOptionSetDefinitions(b0a161bd-409f-ee11-be37-6045bd0064ab)",
+            "Description": {
+                "@odata.type": "Microsoft.Dynamics.CRM.Label",
+                "LocalizedLabels": [
+                {
+                    "@odata.type": "Microsoft.Dynamics.CRM.LocalizedLabel",
+                    "Label": "Choice Attribute",
+                    "LanguageCode": 1033,
+                    "IsManaged": False
+                }
+                ],
+                "UserLocalizedLabel": {
                 "@odata.type": "Microsoft.Dynamics.CRM.LocalizedLabel",
                 "Label": "Choice Attribute",
                 "LanguageCode": 1033,
                 "IsManaged": False
-            }
-            ],
-            "UserLocalizedLabel": {
-            "@odata.type": "Microsoft.Dynamics.CRM.LocalizedLabel",
-            "Label": "Choice Attribute",
-            "LanguageCode": 1033,
-            "IsManaged": False
-            }
-        },
-        "DisplayName": {
-            "@odata.type": "Microsoft.Dynamics.CRM.Label",
-            "LocalizedLabels": [
-            {
+                }
+            },
+            "DisplayName": {
+                "@odata.type": "Microsoft.Dynamics.CRM.Label",
+                "LocalizedLabels": [
+                {
+                    "@odata.type": "Microsoft.Dynamics.CRM.LocalizedLabel",
+                    "Label": "Sample Choice 3.0",
+                    "LanguageCode": 1033,
+                    "IsManaged": False
+                }
+                ],
+                "UserLocalizedLabel": {
                 "@odata.type": "Microsoft.Dynamics.CRM.LocalizedLabel",
                 "Label": "Sample Choice 3.0",
                 "LanguageCode": 1033,
                 "IsManaged": False
-            }
-            ],
-            "UserLocalizedLabel": {
-            "@odata.type": "Microsoft.Dynamics.CRM.LocalizedLabel",
-            "Label": "Sample Choice 3.0",
-            "LanguageCode": 1033,
-            "IsManaged": False
-            }
-        },
-        "RequiredLevel": {
-            "Value": "None",
-            "CanBeChanged": False,
-            "ManagedPropertyLogicalName": "canmodifyrequirementlevelsettings"
-        },
-        "SchemaName": "mow_SampleChoice3"
-    }
+                }
+            },
+            "RequiredLevel": {
+                "Value": "None",
+                "CanBeChanged": False,
+                "ManagedPropertyLogicalName": "canmodifyrequirementlevelsettings"
+            },
+            "SchemaName": "mow_SampleChoice3"
+        }
 
-    # Json model of headers needed to post to projects table in Sandbox_Mowery
-    post_headers = headers = {
-        'MSCRM.SolutionName': 'MoweryCRM',
-        'OData-MaxVersion': '4.0',
-        'OData-Version': '4.0',
-        'If-None-Match': 'null',
-        'Accept': 'application/json',
-        'Content-Type': 'application/json; charset=utf-8',
-        'Authorization': 'Bearer ' + token['access_token'],
-    }
-    
-    return token
-
-def post_attirbute(token, post_headers, post_body):
-
-    # Existent token
-    if "access_token" in token:
-        print("Token was obtained from:", token["token_source"])  # Since MSAL 1.25
-        # Calling graph using the access token
-        response = requests.post(  # Use token to call downstream service
-            config["endpoint"],
-            headers=post_headers, json=post_body)
+        # Json model of headers needed to post to projects table in Sandbox_Mowery
+        post_headers = headers = {
+            'MSCRM.SolutionName': 'MoweryCRM',
+            'OData-MaxVersion': '4.0',
+            'OData-Version': '4.0',
+            'If-None-Match': 'null',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json; charset=utf-8',
+            'Authorization': 'Bearer ' + token['access_token'],
+        }
         
-        #json_response = response.json()
-        
-        if response.status_code == 200:
-            print("Success!")
-            print(response.headers)
-            #print("Graph API call token: %s" % json.dumps(json_response, indent=2))
+        return token
 
-        elif response.status_code == 401:
-            print("Unauthorized.")
+    def post_attirbute(self, token, post_headers, post_body):
 
+        # Existent token
+        if "access_token" in token:
+            print("Token was obtained from:", token["token_source"])  # Since MSAL 1.25
+            # Calling graph using the access token
+            response = requests.post(  # Use token to call downstream service
+                config["endpoint"],
+                headers=post_headers, json=post_body)
+            
+            #json_response = response.json()
+            
+            if response.status_code == 200:
+                print("Success!")
+                print(response.headers)
+                #print("Graph API call token: %s" % json.dumps(json_response, indent=2))
+
+            elif response.status_code == 401:
+                print("Unauthorized.")
+
+            else:
+                print(response.content)
+
+            
         else:
-            print(response.content)
-
-        
-    else:
-        print("Token acquisition failed")  # Examine token["error_description"] etc. to diagnose error
+            print("Token acquisition failed")  # Examine token["error_description"] etc. to diagnose error
 
