5648bc2f9da0f5b44dca41c7e549d7542b2aa8c5
bashfulmonkey <rearick.seth@gmail.com>
2023-12-21 13:57:50 -0500

    Imported PowerAppEntity to test the functionality

--- Driver.py
+++ Driver.py
@@ -1,12 +1,12 @@
-import PowerAppEntity
+from PowerAppEntity import PowerAppEntity
 import json
 import sys
 
-config = json.load(open(sys.argv[1]))
+configure = json.load(open(sys.argv[1]))
 
 gobal_choice = "b0a161bd-409f-ee11-be37-6045bd0064ab"
 
-table = PowerAppEntity(config, "MoweryCRM")
+table = PowerAppEntity(configure, "MoweryCRM")
 
-table.post_global_choice_attribute("SampleChoice3", "Sample Choice 3.0", gobal_choice)
+#table.post_global_choice_attribute("SampleChoice4", "Sample Choice 4.0", gobal_choice)
 table.post_text_attribute("SampleText", "Sample Text")
