7f7c98a0a56d6bafe48d5df579e98fe9f420fcdc
bashfulmonkey <rearick.seth@gmail.com>
2023-12-21 16:21:58 -0500

    Configured Driver.py for production

--- Driver.py
+++ Driver.py
@@ -2,11 +2,28 @@ from PowerAppEntity import PowerAppEntity
 import json
 import sys
 
+# Configuration file with correct endpoint and environment
 configure = json.load(open(sys.argv[1]))
 
-gobal_choice = "b0a161bd-409f-ee11-be37-6045bd0064ab"
+# Picklist ID
+global_choice = "9de86e6b-45a0-ee11-be37-6045bd006eaf"
 
+# PowerAppEntity object for posting data to PowerApps
 table = PowerAppEntity(configure, "MoweryCRM")
 
-#table.post_global_choice_attribute("SampleChoice4", "Sample Choice 4.0", gobal_choice)
-table.post_text_attribute("SampleText", "Sample Text")
+# JSON file of all attributes: schema_name, label, type
+f = open("./SkillsFields.json", "r", encoding='utf-8')
+
+# Loading file in as JSON
+attributes = json.load(f)
+
+# Loop over every attribute
+for attribute in attributes:
+    
+    # If the attribute is of type choice post a choice attribute
+    if attributes[attribute]["type"] == "choice":
+        table.post_global_choice_attribute(attribute, attributes[attribute]["label"], global_choice)
+
+    # If the attribute is of type text post a memo attribute
+    elif attributes[attribute]["type"] == "text":
+        table.post_memo_attribute(attribute, attributes[attribute])
